9b2021ea7a2dbac98473f4037e95d29ffd506b31
wangxiaojun <13051702443@163.com>
2023-03-09 11:27:34 +0800

    2022

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
 <?php
-echo 1111;die;
-echo 2222;die;
-echo 3333;die;
-echo 4444;die;
-echo 5555;die;
-echo 6666;die;
-echo 1111;
-die;
-echo 2222;
-die;
-echo phpinfo();
-die;
-$a = 2222;
-die;
-$b = 8888;
-die;
